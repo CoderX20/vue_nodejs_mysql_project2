4bab6f655cd6e6c12ee07de5f0d6b6d3a135a7e9
Jim <91730405+CoderX20@users.noreply.github.com>
2022-05-20 10:22:29 +0800

    first commit

---
+++ node_login1.js
@@ -0,0 +1,43 @@
+const express=require('express')
+const fs=require('fs')
+const path=require('path')
+const mysql=require('mysql')
+const time=require('./node_time_set1')
+const login_router=express.Router()
+
+var database_config={
+    host:'127.0.0.1',
+    port:'3306',
+    user:'root',
+    password:'gx628572',
+    database:'my_database1'
+}
+
+
+
+login_router.post('/loginCheck',function(request,response){
+    console.log(request.method+'>'+request.url+":"+time.get_time_now())
+    var url_query=request.query
+    var check_db=mysql.createConnection(database_config)
+    var query_sql_str='select * from students where name=? and password=?'
+    check_db.connect()
+    check_db.query(query_sql_str,[url_query.account,url_query.password],function(err,res_data){
+        if(err){
+            console.log(err.message)
+            response.send({result:'false'})
+            throw err
+        }
+        else{
+            // console.log(res_data)
+            if(res_data.length==0){
+                response.send({result:'false'})
+            }
+            else{
+                response.send({result:'true'})
+            }
+        }
+    })
+    check_db.end()
+})
+
+module.exports=login_router
---
+++ node_register1.js
@@ -0,0 +1,50 @@
+const express=require('express')
+const mysql=require('mysql')
+const time=require('./node_time_set1')
+const register_router=express.Router()
+
+var connect_config={
+    host:'127.0.0.1',
+    port:'3306',
+    user:'root',
+    password:'gx628572',
+    database:'my_database1'
+}
+
+register_router.post('/registerCheck',function(request,response){
+    var url_query=request.query
+    var query_db=mysql.createConnection(connect_config)
+    var query_sql_str="select username from users where username= ?"
+    query_db.connect()
+    query_db.query(query_sql_str,[url_query.account],function(err,result){
+        if(err){
+            console.log(err.message)
+            response.send({result:'false'})
+            throw err
+        }
+        else{
+            if(result.length!=0){
+                response.send({result:'exit'})
+            }
+            else{
+                var insert_db=mysql.createConnection(connect_config)
+                var insert_sql_str="insert into users (username,password,status) values(?,?,?)"
+                insert_db.connect()
+                insert_db.query(insert_sql_str,[url_query.account,url_query.password,1],function(err,result){
+                    if(err){
+                        console.log(err.message)
+                        response.send({result:'false'})
+                        throw err
+                    }
+                    else{
+                        response.send({result:'true'})
+                    }
+                })
+                insert_db.end()
+            }
+        }
+    })
+    query_db.end()
+})
+
+module.exports=register_router
